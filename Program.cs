f66f5898d96ca3f03a853acc2c3cc75bc7d13f9b
gabrielmarcos2 <gabrielsqueda123@gmail.com>
2022-05-20 22:41:44 -0300

    tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("quantos elefantes: ");
+int quantidade = Convert.ToInt32(Console.ReadLine());
+
+if (quantidade <= 2 || quantidade % 2 ==1)
+{
+    Console.WriteLine("\nsomente serão aceitos numeros pares de elefantes, maiores do que 2. ");
+}
+else
+{
+    int elefanteAtuais = 1;
+
+    while (elefanteAtuais <= quantidade)
+    {
+        if (elefanteAtuais == 1)
+        {
+            Console.WriteLine($"\n{elefanteAtuais} elefante incomoda muita gente ");
+        }
+        else
+        {
+            Console.WriteLine($"\n{elefanteAtuais} elefantes incomodam muita gente");
+        }
+
+        elefanteAtuais += 1;
+
+        Console.Write($"{elefanteAtuais} elefantes");
+
+        int incomoda = 1;
+        while (incomoda <= elefanteAtuais)
+        {
+            Console.Write("incomodam ");
+            incomoda += 1;
+        }
+
+        Console.Write("muito mais");
+        elefanteAtuais += 1;
+ }
+}
+
